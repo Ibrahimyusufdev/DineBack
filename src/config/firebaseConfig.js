2203bbbe10dec7800b59f7b5d13d6570e1fa4f45
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-08-23 20:03:59 +0100

    Merged conflicts for firebaseConfig

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAu96QIn_lR9Rvb3PUL-0POMKB0DfFbwp0",
-  authDomain: "dineback-667f3.firebaseapp.com",
-  projectId: "dineback-667f3",
-  storageBucket: "dineback-667f3.firebasestorage.app",
-  messagingSenderId: "383752189812",
-  appId: "1:383752189812:web:19a9b274072cd333265e21",
-  measurementId: "G-HRES3G4PW4"
-=======
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
@@ -26,17 +10,10 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
->>>>>>> 242e13b292e6bdf8b507978b1c1d04640518d210
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
-const analytics = getAnalytics(app);
-// Initialize Auth
-export const auth = getAuth(app);
-=======
 
 // Initialize Auth
 export const auth = getAuth(app);
->>>>>>> 242e13b292e6bdf8b507978b1c1d04640518d210
